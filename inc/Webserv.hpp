659ee7b2f012a927f1197eb6498522e053cfe628
Illia <robot13673@gmail.com>
2025-01-20 20:31:08 +0100

    First commit

---
+++ inc/Webserv.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "../inc/Webserv.hpp"
+
+int main(int argc, char **argv)
+{
+    if(argc == 1 || argc == 2)
+    {
+        //logic
+    }
+    else
+    {
+        std::cerr << "Error: Wrong arguments"; //Change to Logger class
+    }
+    return 0;
+}
