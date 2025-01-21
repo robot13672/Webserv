1a3d8ac0ce241492121359612f8c89357cade237
Illia Khristinchenko <ikhristi@c1r3s5.42wolfsburg.de>
2025-01-21 13:43:14 +0100

    firs server

--- inc/Webserv.hpp
+++ inc/Webserv.hpp
@@ -1,10 +1,29 @@
 #pragma once
 
-#include <iostream>
+//standart libs
+#include <iostream>// для логирования, и работы с консолью
 #include <string>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <vector>
+#include <map>
+#include <set>
+#include <exception> // для работы с исключениями
+#include <sstream> //для форматирования строк при генерации HTTP ответов
+
+//Системные библиотеки POSIX
+#include <sys/socket.h> // для работы с сокетами
+#include <netinet/in.h> // для описания структуры адресации и работы с IP-адресами
+#include <unistd.h> // для системных вызовов close() и для read(), write()
+#include <fcntl.h> // для управления дескрипторами файлов
+#include <signal.h> // для обработки сигналов
+#include <cstring> // для работы с С-строками
+#include <errno.h> // для обработки ошибок, связанных с системными вызовами
+
+//Пользовательские библиотеки
+//e.g.  Logger
+
+
+//Библиотеки для CGI
+
+
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,37 @@ int main(int argc, char **argv)
     if(argc == 1 || argc == 2)
     {
         //logic
+        int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_fd == -1)
+        {
+            std::cerr << "Error: Error with socket";
+            return (1);
+        }
+        sockaddr_in address = {0};
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;   
+        address.sin_port = htons(8080);
+
+        if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)))
+        {
+            std::cerr << "Error: Error with bind";
+            return (1);
+        }
+        listen(server_fd, 10);
+        std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!"; // ответ
+        while(1)
+        {
+            int client_fd = accept(server_fd, nullptr, nullptr); //изменить nullptr, если нужно хранить информацию про клиента
+            if(client_fd == -1)
+            {
+                std::cerr << "Error: Error with listening";
+                return (1);
+            }
+            std::cout << "Client connected: " << client_fd << std::endl;
+            send(client_fd, response.c_str(), response.size(), 0);
+            close(client_fd);
+        }
+        close(server_fd);
     }
     else
     {
