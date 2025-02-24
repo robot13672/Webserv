a8121f4c12474ab73829762557206cbd3ed3faa0
Oleh Ruban <polihronii@gmail.com>
2025-02-24 18:34:55 +0100

    defult.conf locattion added into main()

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,50 @@
 
 void signalHandler(int signum) {
 
-    logger.closeFile();
+	logger.closeFile();
 
-    exit(signum);  // Завершаем программу
+	exit(signum);  // Завершаем программу
 }
 
 int main(int argc, char **argv)
 {
 	ServerConfig a;
-    if(argc == 1 || argc == 2)
-    {
-        try
-        {
-			a.parseConfig(std::string(argv[1])); // roi 0224 Явное преобразование char* в std::string
-			std::cout << a << std::endl; // roi 0224
+	std::string confLoc;
+	if(argc == 1 || argc == 2)
+	{
+		try
+		{
+			/* roi 0224 - insert start */
+			if (argc == 1)
+				confLoc = "congifs/default.conf";
+			else
+				confLoc = std::string(argv[1]);
+			a.parseConfig(confLoc); // roi 0224 Явное преобразование char* в std::string
+			std::cout << a << std::endl; // debug
+			/* roi 0224 - insert end */
 
 			signal(SIGINT, signalHandler);
-            logger.setFile("logger.txt");//нужно будет ставить реальный с конфига
-            Server Webservers;
-            std::vector<ServerConfig> servers;
-            servers.push_back(ServerConfig("127.0.0.1", 8084));
-            servers.push_back(ServerConfig("127.0.0.1", 8085));
-            servers.push_back(ServerConfig("127.0.0.1", 8084));
-            Webservers.setupServer(servers);
-            Webservers.startServers();
-            
-        }
-        catch(const std::exception& e)
-        {
-            // std::cerr << e.what() << std::endl; // roi 0224 - temporary to check fileopen error
+			logger.setFile("logger.txt");//нужно будет ставить реальный с конфига
+			Server Webservers;
+			std::vector<ServerConfig> servers;
+			servers.push_back(ServerConfig("127.0.0.1", 8084));
+			servers.push_back(ServerConfig("127.0.0.1", 8085));
+			servers.push_back(ServerConfig("127.0.0.1", 8084));
+			Webservers.setupServer(servers);
+			Webservers.startServers();
+			
+		}
+		catch(const std::exception& e)
+		{
+			// std::cerr << e.what() << std::endl; // roi 0224 - temporary to check fileopen error
 			logger.writeMessage(e.what());
-            return 1;
-        }
-       
-    }
-    else
-    {
-        logger.writeMessage("Error: Wrong arguments");
-    }
-    return 0;
+			return 1;
+		}
+	   
+	}
+	else
+	{
+		logger.writeMessage("Error: Wrong arguments");
+	}
+	return 0;
 }
